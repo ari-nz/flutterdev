3a35bea67952b7297413e106d467a6ea39030fcd
ari-nz <hello@ari.nz>
2021-11-06 22:51:18 +1300

    Stateless basic widget

--- aricard/lib/main.dart
+++ aricard/lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(baseApp());
+}
+
+class baseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.green,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
