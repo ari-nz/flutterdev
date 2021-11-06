77a7ec61fbbee74304cf9f97caa6c4e7cf48d2ee
ari-nz <hello@ari.nz>
2021-11-06 21:34:17 +1300

    image fixes

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("I am Ari"),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-                "https://ari.nz/home/profile_hu3e6ade83d4c002649d96cf3ab5ff55c8_41754_320x0_resize_q75_box.jpg"),
-          ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("I am Ari"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
         ),
       ),
     ),
-  );
+  ));
 }
