7d21a7c101d355315067593352896ea0bc90eba7
ari-nz <hello@ari.nz>
2021-11-13 15:58:04 +1300

    Completed layout challenge

--- aricard/lib/main.dart
+++ aricard/lib/main.dart
@@ -9,8 +9,41 @@ class baseApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.green,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100,
+              ),
+              Container(
+                child: Column(
+                  // crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.yellow,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.green,
+                      width: 100,
+                      height: 100,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
